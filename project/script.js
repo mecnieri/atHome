4330e767afcb85fea2b10dc31aef4a0eafbd03c2
mecnieri <abzogio@yahoo.com>
2018-07-05 18:19:04 +0400

    564

--- project/script.js
+++ project/script.js
@@ -14,48 +14,48 @@ let params = {
     pAttValue: "trainingDay ",
 
     elementTagName: "div",
-    attName: "class",
-    attValue: "green",
+    attName: "id",
+    attValue: "student_TrnDay_",
     count: 16,
 }
 
 document.getElementById("addDaybutton").addEventListener("click", function () {
 
     a++;
-     
-     //#region date 
+
+    //#region date 
 
     if (a % 4 == 1 || a % 4 == 2 || a % 4 == 3) {
         b += 2;
     }
-    
+
     else if (a % 4 == 0) {
         b++;
     }
-     
+
     let apr = new Date(2018, 3, 28 + b, )
     let day = days[apr.getDay()];
     let date = apr.getDate();
     let month = months[apr.getMonth()];
     let newdate = day + " </br> " + month + " " + date;
 
- 
+
     //#endregion 
 
 
     function addingColumn(arg) {
-        
+
         let grandParentClass = document.querySelector(arg.grandParentSelector);
         let parentNode = document.createElement(arg.parentTagName);
         grandParentClass.appendChild(parentNode).setAttribute(arg.pAttName, arg.pAttValue + a);
 
- 
+
         for (let i = 0; i < arg.count; i++) {
 
             let parentId = document.querySelector(".table").lastElementChild;
             let elementNode = document.createElement(arg.elementTagName);
             let elementText = document.createTextNode("0");
-            parentId.appendChild(elementNode).setAttribute(arg.attName, arg.attValue + i);
+            parentId.appendChild(elementNode).setAttribute(arg.attName, arg.attValue + i + "_" + a);
             elementNode.appendChild(elementText);
         }
 
@@ -65,30 +65,30 @@ document.getElementById("addDaybutton").addEventListener("click", function () {
     addingColumn(params);
 
 
- 
+
 });
 
 document.getElementById("removeDay").addEventListener("click", function () {
- 
+
     // Removes an element from the document
     if (a % 4 == 1 || a % 4 == 2 || a % 4 == 3) {
         b -= 2;
     }
-    
+
     else if (a % 4 == 0) {
         b--;
     }
-    
+
     let apr = new Date(2018, 3, 28 + b, )
     let day = days[apr.getDay()];
     let date = apr.getDate();
     let month = months[apr.getMonth()];
     let newdate = day + " " + month + " " + date;
-    
+
     // console.log(b);
     console.log(newdate);
     console.log(apr.getDay());
-    a--; 
+    a--;
     let element = document.querySelector(".table").lastElementChild;
     element.parentNode.removeChild(element);
 
