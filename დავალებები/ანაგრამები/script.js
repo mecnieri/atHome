1593c2f960e29481ff4694322ced01520a5ddc0a
mecnieri <abzogio@yahoo.com>
2018-07-31 03:53:11 +0400

    edasd

--- დავალებები/ანაგრამები/script.js
+++ დავალებები/ანაგრამები/script.js
@@ -1,28 +1,23 @@
 let a = prompt("Type two words and I will tell you, are they anagrams or not ? \nPlease, type here first word")
-let b = a.split("")
-let c = b.sort()
+let b = a.split("").sort()
 
-console.log(c.length);
-let d = prompt("Type here second word")
-let e = d.split("")
-let f = e.sort()
-f
-console.log(d.length);
-let x = 0;
-if (a.length === d.length) {
+let c = prompt("Type here second word")
+let d = c.split("").sort()
 
-    for (let i = 0; i < c.length; i++) {
-        if (c[i] === f[i]) {
-            x++
-            x
+let countCoincidence = 0;
+
+if (a.length === c.length) {
+    for (let i = 0; i < a.length; i++) {
+        if (b[i] === d[i]) {
+            countCoincidence++;
         } else {
-            alert(`sorry, "${a}" and "${d}" are NOT anagrams !`);
+            alert(`sorry, "${a}" and "${c}" are NOT anagrams !`);
         }
     }
 }
 else {
-    alert(`sorry, "${a}" and "${d}" are NOT anagrams !`);
+    alert(`sorry, "${a}" and "${c}" are NOT anagrams !`);
 }
-if(x === a.length){
-    alert(`CONGRATS! "${a}" and "${d}" are anagrams !`)
+if (countCoincidence === a.length) {
+    alert(`CONGRATS! "${a}" and "${c}" are anagrams !`)
 }
