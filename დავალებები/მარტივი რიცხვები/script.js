f722112d7f34a3dd056d632e123bf7f9b15ede22
mecnieri <abzogio@yahoo.com>
2018-08-01 03:27:57 +0400

    asda

---
+++ დავალებები/მარტივი რიცხვები/script.js
@@ -0,0 +1,20 @@
+
+let firstNumber = Number(prompt("insert first number here"));
+let lastNumber = Number(prompt("insert last number here"));
+
+let totalPrimeNumbers = 0;
+for (let x = firstNumber; x <= lastNumber; x++) {
+    let b = 0;
+    for (let i = 0; i < x + 1; i++) {
+
+        if (x % i == 0) {
+            b++;
+        }
+    }
+    if (b == 2) {
+        document.write("<br> &emsp;" + x);
+        totalPrimeNumbers++;
+    }
+}
+
+document.write(`<br><br>There are ${totalPrimeNumbers} prime numbers between ${firstNumber} and ${lastNumber}`);
