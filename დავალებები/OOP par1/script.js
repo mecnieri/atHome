1e74ace2041754a33ee05a19202719cf652bd0d0
mecnieri <abzogio@yahoo.com>
2018-08-03 16:57:37 +0400

    qwe

--- დავალებები/OOP par1/script.js
+++ დავალებები/OOP par1/script.js
@@ -33,28 +33,103 @@
 
 // EXERCISE 5
 
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this;
-    },
-    down() {
-        this.step--;
-    },
-    q() {
-        this.step++;
-        return this;
-    },
-    showStep: function () { // shows the current step
-        alert(this.step);
-    }
-};
+// let ladder = {
+//     step: 0,
+//     up() {
+//         this.step++;
+//         return this;
+//     },
+//     down() {
+//         this.step--;
+//     },
+//     q() {
+//         this.step++;
+//         return this;
+//     },
+//     showStep: function () { // shows the current step
+//         alert(this.step);
+//     }
+// };
 
 // ladder.up();
 // ladder.up();
 // ladder.down();
 // ladder.showStep(); //
 
-ladder.up().up().up().up().q().down()
-ladder.showStep();
+// ladder.up().up().up().up().q().down()
+// ladder.showStep();
+
+
+// EXERCISE 6  constructor function
+
+// function Calculator() {
+
+//     this.read = function () {
+//         this.num1 = prompt("here")
+//         this.num2 = prompt("aseve")
+//     },
+//         this.sum = function () {
+//             return Number(this.num1) + Number(this.num2);
+//         },
+//         this.mul = function () {
+//             return this.num1 * this.num2;
+//         }
+// }
+
+// let calculator = new Calculator()
+// calculator.read();
+// alert("Sum = " + calculator.sum());
+// alert("Mul = " + calculator.mul());
+
+
+// EXERCISE 7
+
+
+// function Accumulator(startingValue) {
+//     this.value = Number(startingValue);
+//     this.read = function () {
+//         this.num1 = prompt("here")
+//         this.value += Number(this.num1)
+
+//     }
+
+// }
+
+// let accumulator = new Accumulator(1);
+// accumulator.read();
+// accumulator.read();
+
+// alert(accumulator.value);
+
+
+// EXERCISE 8 
+function Calculator() {
+    this.addOperator = function (name, func) {
+        return {
+            [name]: func,
+        }
+    }
+    this.calculate = function (str) {
+        b = str.split(" ")
+        if (b[1] == "+") {
+            let c = Number(b[0]) + Number(b[2])
+            console.log(c);
+        }
+        else if (b[1] == "-") {
+            let c = Number(b[0]) - Number(b[2])
+            console.log(c);
+        } else {
+            let op = b[1];
+            for (let key in this.addOperator(op)) {
+                console.log(this.addOperator(op, (a, b) => a * b)[key](b[0], b[2]));
+
+            }
+        }
+    }
+}
+
+
+let powerCalc = new Calculator()
+powerCalc.addOperator("*", (a, b) => a * b);
+powerCalc.addOperator("/", (a, b) => a / b);
+powerCalc.calculate("12 / 3");
