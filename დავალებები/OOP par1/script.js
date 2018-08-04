995bfdb2b340d7b0efe77ccd6acd90674266ec3c
mecnieri <abzogio@yahoo.com>
2018-08-04 04:32:48 +0400

    qe

--- დავალებები/OOP par1/script.js
+++ დავალებები/OOP par1/script.js
@@ -102,34 +102,35 @@
 // alert(accumulator.value);
 
 
-// EXERCISE 8 
-function Calculator() {
-    this.addOperator = function (name, func) {
-        return {
-            [name]: func,
-        }
-    }
-    this.calculate = function (str) {
-        b = str.split(" ")
-        if (b[1] == "+") {
-            let c = Number(b[0]) + Number(b[2])
-            console.log(c);
-        }
-        else if (b[1] == "-") {
-            let c = Number(b[0]) - Number(b[2])
-            console.log(c);
-        } else {
-            let op = b[1];
-            for (let key in this.addOperator(op)) {
-                console.log(this.addOperator(op, (a, b) => a * b)[key](b[0], b[2]));
+// // EXERCISE 8 
+// function Calculator() {
+//     this.addOperator = function (name, func) {
+//         this.addOperator[name] = func
+//     }
+//     this.calculate = function (str) {
+//         b = str.split(" ")
+//         if (b[1] == "+") {
+//             let c = Number(b[0]) + Number(b[2])
+//             console.log(c);
+//         }
+//         else if (b[1] == "-") {
+//             let c = Number(b[0]) - Number(b[2])
+//             console.log(c);
+//         } else {
 
-            }
-        }
-    }
-}
+//             for (key in this.addOperator) {
+//                 if (key === b[1]) {
+//                     console.log(this.addOperator[key](b[0], b[2]));
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
-let powerCalc = new Calculator()
-powerCalc.addOperator("*", (a, b) => a * b);
-powerCalc.addOperator("/", (a, b) => a / b);
-powerCalc.calculate("12 / 3");
+// let powerCalc = new Calculator()
+
+// // powerCalc.addOperator("*", (a, b) => a * b);
+// powerCalc.addOperator("/", (a, b) => a / b);
+// powerCalc.addOperator("**", (a, b) => a ** b)
+// powerCalc.calculate("2 ** 4");
---
+++ დავალებები/extending calculator/script.js
@@ -0,0 +1,32 @@
+// EXERCISE 8 
+function Calculator() {
+    this.addOperator = function (name, func) {
+        this.addOperator[name] = func
+    }
+    this.calculate = function (str) {
+        b = str.split(" ")
+        if (b[1] == "+") {
+            let c = Number(b[0]) + Number(b[2])
+            console.log(c);
+        }
+        else if (b[1] == "-") {
+            let c = Number(b[0]) - Number(b[2])
+            console.log(c);
+        } else {
+
+            for (key in this.addOperator) {
+                if (key === b[1]) {
+                    console.log(this.addOperator[key](b[0], b[2]));
+                }
+            }
+        }
+    }
+}
+
+
+let powerCalc = new Calculator()
+
+// powerCalc.addOperator("*", (a, b) => a * b);
+powerCalc.addOperator("/", (a, b) => a / b);
+powerCalc.addOperator("**", (a, b) => a ** b)
+powerCalc.calculate("2 ** 4");
